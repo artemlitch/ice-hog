810dce0c7d8ac2a52b3c2071e2bd199f22511235
Haseeb Saeed <hs17@hotmail.co.uk>
2016-07-13 10:49:11 -0400

    Small code cleanup

--- dom.cc
+++ dom.cc
@@ -13,13 +13,13 @@ void TextNode::prettyPrint() {
 void ElemNode::prettyPrint() {
     cout << "<" << this->tag_name << " ";
 
-    for(auto attr : attributes) {
+    for(const auto& attr : attributes) {
         cout << attr.first << "=" << attr.second << " ";
     }
     cout << ">" << endl;
 
-    for(Node* child : children) {
+    for(auto child : children) {
         child->prettyPrint();
     }
-    cout <<endl<< "</" << this->tag_name << ">" << endl;
+    cout << endl << "</" << this->tag_name << ">" << endl;
 }
